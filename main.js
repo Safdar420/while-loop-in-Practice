68341a00b7f7c363cddd582f5fed1b2dfcdff960
Safdar420 <160203243+Safdar420@users.noreply.github.com>
2024-05-09 01:05:11 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+//  While loop in practice 
+//  Q no 1
+// let favouriteDishes:string[]=["Biryani","Nihari","Haleem","Pulao"]
+// let i=0;
+// while( i <= 4){
+//        console.log(`my Favourite dishe is ${favouriteDishes[0]} and ${favouriteDishes[1]}`);
+//        i++
+// }
+// //  Q no 2
+// let programmingLanguages:string[] =["Javascript","Typescript","Python","C","C++"]
+// let a=0;
+// while( a <=5){
+//        console.log(`My Favourite Programming Language is ${programmingLanguages[1]} and ${programmingLanguages[2]}`);
+//        a++
+// }
+//  Q no 3
+var numbers = [1, 2, 3, 4, 5];
+var b = 0;
+while (b < 3) {
+    console.log("My Favourite number is ".concat(numbers[4]));
+    b++;
+}
